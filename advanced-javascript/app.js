b9cc732331b2aaeb0f1066072fe4979006a2748b
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-04-08 22:27:25 +0900

    DOM과 그 활용을 배우기 위한 advenced-javascript 모듈

---
+++ advanced-javascript/app.js
@@ -0,0 +1,4 @@
+// console.log(document.body.children[1].children[0].href)
+
+document.body.children[1].children[0].href = 'https://www.google.com';
+// console.log(document.body.children[1].children[0].href)
