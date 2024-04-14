d3de5fa82c03b97da1c11e3b3918da8b2c57e0be
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-04-14 19:05:49 +0900

    express 설치 및 동적 페이지 구성 추가.

--- 11-node-js/app.js
+++ 11-node-js/app.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const { stringify } = require("querystring");
 
 const app = express();
 
@@ -13,7 +12,7 @@ app.get("/currenttime", function (req, res) {
 
 app.get("/", function (req, res) {
   res.send(
-    "<form action='store-user' method='POST'><label>Your Name</label><input type='text' name='username'><button>Submit</button></form>"
+    "<form action='store-user' method='POST'><label>Your Name: </label><input type='text' name='username'><button>Submit</button></form>"
   );
 });
 
@@ -30,6 +29,24 @@ app.post("/store-user", function (req, res) {
   fs.writeFileSync(filePath, JSON.stringify(existingUsers));
 
   res.send("<h1>Username stored!</h1>");
+});
+
+app.get("/users", function (req, res) {
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let responseData = "<ul>";
+
+  for (const user of existingUsers) {
+    responseData += "<li>" + user + "</li>";
+  }
+
+  responseData += '</ul>';
+
+  res.send(responseData)
+
 });
 
 app.listen(3000);
